8bbfb7c6ee2b58361e4f949e609d3cf476cece56
mohiitt <mohitbarade1@gmail.com>
2024-07-06 14:11:20 +0530

    initial commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='mohit123'
+    )
+ #prepare a cursor object 
+cursorObject=dataBase.cursor()
+
+#create a database
+cursorObject.execute("CREATE DATABASE db")
+print("all done !")
